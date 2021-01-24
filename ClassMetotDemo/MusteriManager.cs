8c54e27c0a3e47ae3a7a88f35df70024c2c9bf4f
Okan Erkocu <okanerkocu@hotmail.com>
2021-01-24 23:40:55 +0300

    Customer for banks

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri " + musteri.Adi+ " eklendi.");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri numarası = " + musteri.Id);
+                Console.WriteLine("Müşteri Adı = " + musteri.Adi);
+                Console.WriteLine("Bakiye = " + musteri.Bakiye);
+                Console.WriteLine("------------------------");
+            }
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri " + musteri.Adi + " silindi.");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,29 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Okan";
+            musteri1.Soyadi = "Erk";
+            musteri1.TcNo = "111111111";
+            musteri1.Bakiye = 5000;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Özge";
+            musteri2.Soyadi = "By";
+            musteri2.TcNo = "121211122";
+            musteri2.Bakiye = 3000;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            Console.WriteLine("-------------");
+            musteriManager.Listele(musteriler);
+            musteriManager.Sil(musteri2);
+
         }
     }
 }
